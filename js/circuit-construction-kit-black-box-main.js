045e936494223de901f521da1dbeca1645b31b28
samreid <reids@colorado.edu>
2016-05-06 10:12:30 -0600

    Renamed circuit-construction-kit-basics to circuit-construction-kit, see https://github.com/phetsims/circuit-construction-kit/issues/83

--- js/circuit-construction-kit-black-box-main.js
+++ js/circuit-construction-kit-black-box-main.js
@@ -34,7 +34,7 @@ define( function( require ) {
     }, simOptions );
   }
 
-  // Circuit Construction Kit: Basics has unit tests for checking the mathematics for the Modified Nodal Analysis
+  // Circuit Construction Kit has unit tests for checking the mathematics for the Modified Nodal Analysis
   // algorithm.  In order to load the classes into an accessible namespace, the *-config.js and *-main.js are loaded
   // however, when running the unit tests we don't also want to launch the simulation.
   if ( !window.circuitConstructionKitTestSuite ) {
