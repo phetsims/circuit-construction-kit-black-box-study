012a92c24be37142d5e16c9649243a1850be7865
Sam Reid <reids@colorado.edu>
2021-11-11 22:36:32 -0700

    Regenerate string file with grunt modulify, see https://github.com/phetsims/chipper/issues/1139

--- js/circuitConstructionKitBlackBoxStudyStrings.ts
+++ js/circuitConstructionKitBlackBoxStudyStrings.ts
@@ -3,11 +3,17 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import circuitConstructionKitBlackBoxStudy from './circuitConstructionKitBlackBoxStudy.js';
 
-const circuitConstructionKitBlackBoxStudyStrings = getStringModule( 'CIRCUIT_CONSTRUCTION_KIT_BLACK_BOX_STUDY' );
+type StringsType = {
+  'circuit-construction-kit-black-box-study': {
+    'title': string
+  }
+};
+
+const circuitConstructionKitBlackBoxStudyStrings = getStringModule( 'CIRCUIT_CONSTRUCTION_KIT_BLACK_BOX_STUDY' ) as StringsType;
 
 circuitConstructionKitBlackBoxStudy.register( 'circuitConstructionKitBlackBoxStudyStrings', circuitConstructionKitBlackBoxStudyStrings );
 
