d6cc2fc6843b528e7a6a20fd357385c363c186e3
phet-dev <phetadmin@gmail.com>
2022-01-29 03:19:48 -0700

    update copyright dates from daily grunt work

--- js/blackbox/model/BlackBoxSceneModel.js
+++ js/blackbox/model/BlackBoxSceneModel.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2021, University of Colorado Boulder
+// Copyright 2016-2022, University of Colorado Boulder
 // TODO: Review, document, annotate, i18n, bring up to standards
 
 /**
