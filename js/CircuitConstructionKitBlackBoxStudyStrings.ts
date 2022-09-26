13e35af655b4258f168a2e7327b0c6d25cc25387
Sam Reid <reids@colorado.edu>
2022-09-26 12:11:17 -0600

    Change TReadOnlyProperty => LinkableProperty, see https://github.com/phetsims/axon/issues/414

--- js/CircuitConstructionKitBlackBoxStudyStrings.ts
+++ js/CircuitConstructionKitBlackBoxStudyStrings.ts
@@ -5,13 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
+import LinkableProperty from '../../axon/js/LinkableProperty.js';
 import circuitConstructionKitBlackBoxStudy from './circuitConstructionKitBlackBoxStudy.js';
 
 type StringsType = {
   'circuit-construction-kit-black-box-study': {
     'title': string;
-    'titleStringProperty': TReadOnlyProperty<string>;
+    'titleStringProperty': LinkableProperty<string>;
   }
 };
 
